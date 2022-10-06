762d1b97529c78bac8264e81df785c8c3ccc9831
MB <MB@DESKTOP-BLJ7COK>
2022-10-07 00:25:31 +0530

    //

--- Maven_ajio/src/main/java/pages/CheckSizeChart.java
+++ Maven_ajio/src/main/java/pages/CheckSizeChart.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class CheckSizeChart {
-
+//
 	@FindBy(xpath="//div[@class='size-guide-text']")
 	private  WebElement size_chart;
 	
